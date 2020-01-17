719d4e6665dd447a7e9a946a968d4472b88c81c7
Stanislaw Jaromin <jaromin@onet.pl>
2020-01-17 20:47:03 +0100

    Nowe cwiczenie - kolejny wpis

---
+++ dictionaries.py
@@ -0,0 +1,28 @@
+fruit = {"orange": "a sweet, orange, citrus fruit",
+         "apple": "good for making cider",
+         "lemon": "a sour, yellow citrus fruit",
+         "grape": "a small, sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit"}
+
+
+#dodawanie kolejnych pozycji do słownika (dictionary)
+# print(fruit)
+# print(fruit["lemon"])
+# fruit["pear"] = "an odd shaped apple"
+# print(fruit)
+# fruit["lime"] = "great with tequila"
+# print(fruit)
+# # del fruit["lemon"]
+# # fruit.clear()
+# # print(fruit)
+# print(fruit["tomato"])
+print(fruit)
+while True:
+    dict_key = input("Please enter a fruit: ")
+    if dict_key == "quit":
+        break
+    if dict_key in fruit:
+        description = fruit.get(dict_key)
+        print(description)
+    else:
+        print("we don't have a " + dict_key)
